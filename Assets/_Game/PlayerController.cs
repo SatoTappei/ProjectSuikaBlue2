595e25403a573a25937cc75e1b61be5ed3782498
SatouTappei <stutpi.signup@gmail.com>
2024-01-14 00:56:22 +0900

    じぴてとの連携

--- Assets/_Game/PlayerController.cs
+++ Assets/_Game/PlayerController.cs
@@ -3,36 +3,80 @@ using System.Collections.Generic;
 using UnityEngine;
 using Cysharp.Threading.Tasks;
 using System.Threading;
+using UniRx;
+using UniRx.Triggers;
 
-public class PlayerController : MonoBehaviour
+public struct PlayerControlMessage
 {
-    [SerializeField] Rigidbody _rigidbody;
+    public KeyCode Key;
+}
 
-    void Start()
-    {
-        UpdateAsync(this.GetCancellationTokenOnDestroy()).Forget();
-    }
+/// <summary>
+/// Rotationの値が(0,0,0)の時のForwardを北とした東西南北
+/// </summary>
+public enum PlayerForward
+{
+    East,
+    West,
+    South,
+    North,
+}
 
-    async UniTaskVoid UpdateAsync(CancellationToken token)
+namespace PSB.Game
+{
+    public class PlayerController : MonoBehaviour
     {
-        while (!token.IsCancellationRequested)
+        [SerializeField] Rigidbody _rigidbody;
+        [SerializeField] Transform _eye;
+
+        public bool OnFloorBorder { get; private set; }
+        public PlayerForward Forward { get; private set; }
+
+        void Start()
         {
-            Vector2Int input = default;
-            if (Input.GetKeyDown(KeyCode.A)) input.x--;
-            if (Input.GetKeyDown(KeyCode.S)) input.y--;
-            if (Input.GetKeyDown(KeyCode.D)) input.x++;
-            if (Input.GetKeyDown(KeyCode.W)) input.y++;
+            UpdateAsync(this.GetCancellationTokenOnDestroy()).Forget();
+        }
 
-            if (input == Vector2Int.zero) { await UniTask.Yield(token); continue; }
-
-            for (int i = 0; i <= 60; i++)
+        async UniTaskVoid UpdateAsync(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
             {
-                _rigidbody.velocity = new Vector3(input.x, 0, input.y);
-                await UniTask.Yield(PlayerLoopTiming.FixedUpdate, token);
-            }
-            _rigidbody.velocity = Vector3.zero;
+                // メッセージが飛んでくるまで待機
+                PlayerControlMessage msg = await MessageBroker.Default
+                    .Receive<PlayerControlMessage>().ToUniTask(useFirstValue: true, token);
 
-            await UniTask.Yield(token);
+                // 入力
+                Vector2Int input = default;
+                if (msg.Key == KeyCode.A) { input.x--; Forward = PlayerForward.West; }
+                if (msg.Key == KeyCode.S) { input.y--; Forward = PlayerForward.South; }
+                if (msg.Key == KeyCode.D) { input.x++; Forward = PlayerForward.East; }
+                if (msg.Key == KeyCode.W) { input.y++; Forward = PlayerForward.North; }
+                // 入力あるまでこんてぬ
+                if (input == Vector2Int.zero) { await UniTask.Yield(token); continue; }
+
+                // 入力に応じて1回移動
+                transform.forward = new Vector3(input.x, 0, input.y);
+                for (int i = 0; i <= 60; i++)
+                {
+                    _rigidbody.velocity = new Vector3(input.x, 0, input.y);
+                    await UniTask.Yield(PlayerLoopTiming.FixedUpdate, token);
+                }
+                _rigidbody.velocity = Vector3.zero;
+
+                // レイキャストでステージの縁に立っているか判定
+                OnFloorBorder = Physics.Raycast(_eye.transform.position, _eye.forward + Vector3.down, 1);
+
+                await UniTask.Yield(token);
+            }
+        }
+
+        void OnDrawGizmos()
+        {
+            if (_eye != null)
+            {
+                Vector3 ray = _eye.forward + Vector3.down;
+                Gizmos.DrawRay(_eye.transform.position, ray);
+            }
         }
     }
 }
